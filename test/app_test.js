d66a4ddfe1256690f2692aa6ff7601581b334d15
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-04-13 09:44:10 +0200

    Mocha test for GET response on /api

---
+++ test/app_test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const app = require('../app');
+const request = require('supertest');
+
+describe('The Express app', () => {
+    it('handles GET request to /api', (done) => {
+        request(app)
+            .get('/api')
+            .end((err, res) => {
+                assert(res.body.hi = 'there');
+                done();
+            });
+    });
+});
