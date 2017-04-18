a878ab8ac7e1f22a2a60d9c37a92f21bcfe23d18
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-04-18 16:55:57 +0200

    Ensuring index after collection drop

--- test/test_helper.js
+++ test/test_helper.js
@@ -17,6 +17,7 @@ beforeEach(done => {
     // const drivers = mongoose.connection.collections.drivers;
     const { drivers } = mongoose.connection.collections;
     drivers.drop()
+        .then(() => drivers.ensureIndex({ 'geometry.coordinates': '2dsphere' }))
         .then(() => done())
         .catch(() => done());
 });
